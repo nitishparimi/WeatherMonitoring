a9ce5c7e6cbb01ddc9542938adb34b6dfb24c1f1
Nitish Parimi <2100031401@kluniversity.in>
2024-10-26 21:21:10 +0530

    Update HourlyWeatherDataService.java

--- src/main/java/com/example/demo/HourlyWeatherDataService.java
+++ src/main/java/com/example/demo/HourlyWeatherDataService.java
@@ -22,21 +22,17 @@ public class HourlyWeatherDataService {
         for (String city : cities) {
         	WeatherData weatherData = weatherApiService.getWeatherForCity(city);
 
-        	// Get temperature from "main"
         	double temperature = weatherData.getMain().getTemp() - 273.15;
         	temperature = Math.ceil(temperature);;
 
-        	// Get the main weather condition from the first entry in the "weather" array
         	String weatherCondition = weatherData.getWeather()[0].getMain(); 
 
-        	// Now you can set these values in the HourlyWeather entity
         	HourlyWeather hourlyWeather = new HourlyWeather();
         	hourlyWeather.setCity(city);
         	hourlyWeather.setTemperature(temperature);
         	hourlyWeather.setWeatherCondition(weatherCondition);
-        	hourlyWeather.setTimestamp(currentTime); // Set the current timestamp
+        	hourlyWeather.setTimestamp(currentTime); 
 
-        	// Save to repository
         	hourlyWeatherRepository.save(hourlyWeather);
 
         }
